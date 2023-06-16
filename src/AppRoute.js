153e4ea1b5691abe4b1404bd5d06bee447abbbe7
9KICKS <officialjoshua9@gmail.com>
2023-06-16 13:02:28 +0100

    "updated the app"

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -3,6 +3,10 @@ import Register from "./pages/authentication/components/registerUser";
 import Login from "./pages/authentication/components/loginUser";
 import PrivacyPolicy from "./pages/authentication/components/privacyPolicy";
 import TermsOfService from "./pages/authentication/components/termsAndConditions";
+import ForgotPassword from "./pages/authentication/components/forgotPassword";
+import PasswordResetSent from "./pages/authentication/components/passwordResetResponse";
+import PasswordResetConfirmation from "./pages/authentication/components/passwordResetConfirmation";
+import PasswordResetComplete from "./pages/authentication/components/passwordResetComplete";
 
 function AppRoute() {
   return (
@@ -10,9 +14,13 @@ function AppRoute() {
       <Router>
         <Routes>
           <Route path="/" element={<Register />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/terms" element={<TermsOfService />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/password-reset-sent" element={<PasswordResetSent />} />
+          <Route path="/password-reset-confirmation" element={<PasswordResetConfirmation />} />
+          <Route path="/password-reset-complete" element={<PasswordResetComplete />} />
         </Routes>
       </Router>
     </div>
